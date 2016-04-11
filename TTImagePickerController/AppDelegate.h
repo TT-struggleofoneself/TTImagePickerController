f0141589ed3ff25c964177ee3f9194048db3a572
TT-struggleofoneself <tt_code@tt-codedemacbook-pro.local>
2016-04-11 19:00:49 +0800

    Initial Commit

---
+++ TTImagePickerController/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TTImagePickerController
+//
+//  Created by TT_code on 16/4/11.
+//  Copyright © 2016年 TT_code. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TTImagePickerController/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TTImagePickerController
+//
+//  Created by TT_code on 16/4/11.
+//  Copyright © 2016年 TT_code. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
